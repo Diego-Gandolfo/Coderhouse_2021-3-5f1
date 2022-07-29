1ee4c06f98dcff74c070013e59e83bc82073d852
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2022-07-29 15:32:12 -0300

    Ajustes para After

--- Assets/After/Scripts/Scorer.cs
+++ Assets/After/Scripts/Scorer.cs
@@ -6,9 +6,5 @@ public class Scorer : MonoBehaviour
 {
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.KeypadPlus))
-        {
-            GameManager.Instance.score++;
-        }
     }
 }
