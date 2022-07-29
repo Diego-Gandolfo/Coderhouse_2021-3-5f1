83853baa2ec28ccaf5367344fd72f6c4735c2bf9
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2022-07-29 15:16:17 -0300

    Updates

---
+++ Assets/After/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public int score;
+
+    private void Awake()
+    {        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            SceneManager.LoadScene("1");
+        }
+        else if (Input.GetKeyDown(KeyCode.F2))
+        {
+            SceneManager.LoadScene("2");
+        }
+    }
+}
---
+++ Assets/After/Scripts/Scorer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorer : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.KeypadPlus))
+        {
+            // Incrementar el Score
+        }
+    }
+}
